e0f95f35f31f7b0c3240d1485d38c4ec5c671816
*Collins <nyamburycollins@mail.com>
2025-05-14 23:16:33 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,7 @@ from dog import Dog
 import ipdb
 
 ipdb.set_trace()
+ipdb> Dog("Fido", "Corgi")
+<dog.Dog object at 0x1051dc700>
+ipdb> Dog("Fido the Pug who likes to roll in the mud", "Pug")
+*** ValueError: Name must be string between 1 and 25 characters.
